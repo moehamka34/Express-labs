05daf9b26a3df70754738b9a643ef085480f7103
Mohamed Hamka <mohamedhamka@Mohameds-MBP-2.attlocal.net>
2022-05-17 06:40:00 -0400

    homework done

--- server.js
+++ server.js
@@ -1,20 +1,36 @@
-const express = require ( 'express')
+const express = require('express');
 const app = express();
-const greeting = [
-    "Hello, Stranger",
-    "Whats's up, Moe!",
-    "Hey man! How's it going?",
-    "Whats up buddy!"
-    
-]
 
-// greeting route
+const name = [
+    "Hello",
+    "Whats up <name>",
+    "<name> It's so great to see you",
 
-app.get('/:name', (req, res) =>{
-    res.send(greeting[req.params.name]);
+  ]
+
+const ballResponse = 
+["It is certain", "It is decidedly so", "Without a doubt", "Yes definitely","You may rely on it", "As I see it yes", "Most likely", "Outlook good","Yes", "Signs point to yes", "Reply hazy try again", "Ask again later","Better not tell you now", "Cannot predict now", "Concentrate and ask again","Don't count on it", "My reply is no", "My sources say no","Outlook not so good", "Very doubtful"]
+
+
+app.get('/greeting', (req, res) => {
+    res.send('Hello, stranger')
 });
+
+app.get("/:name", (req, res) => {
+    res.send("Whats up " + req.params.name)
+  });
+
+  app.get("/tip/:total/:tipPercentage", (req, res) => {
+    let tip = req.params.total * (req.params.tipPercentage / 100);
+    res.send("" + tip);
+  });
+
+  app.get("/magic/Will%20I%20Be%20A%20Millionaire", (req, res) => {
+    let randomIndex = Math.floor(Math.random() * ballResponse.length)
+    res.send(ballResponse[randomIndex])
+  });
 
 
 app.listen(3000, () => {
-    console.log('Hello, there');
-})
+    console.log('Express is listening for requests from the browser on port 3000');
+});
